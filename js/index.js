e814f74faa991e246393d9b777f93b26ad1b7a46
bek-211203-coder <bek406197@gmail.com>
2024-08-08 14:08:13 +0500

    new

--- js/index.js
+++ js/index.js
@@ -25,9 +25,15 @@ window.onscroll = () => {
             navLinks.forEach(navLinks => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*='+ +']').classList.add('active')
-            })
+            });
+            // active sections for animation on scroll
+            sec.classList.add('show-animate');
         }
-    })
+        // if want to use animation that repeats on scroll use this
+        else{
+            sec.classList.remove ('show-animate');
+        }
+    });
 
     // sticky header
     let header = document.querySelector('header');
